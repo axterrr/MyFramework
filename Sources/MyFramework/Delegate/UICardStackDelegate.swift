b5d242ccbc0c91fa2d41cf6a273237b0969bd8bc
Vladyslav Hibskyi <vgibskiy@gmail.com>
2025-11-23 12:01:47 +0200

    start implementing

--- Sources/MyFramework/Delegate/UICardStackDelegate.swift
+++ Sources/MyFramework/Delegate/UICardStackDelegate.swift
@@ -4,3 +4,8 @@ public protocol UICardStackDelegate: AnyObject {
     func cardStack(_ cardStack: UICardStackView, didSwipeCardAt index: Int, with direction: SwipeDirection)
     func cardStackDidRunOutOfCards(_ cardStack: UICardStackView)
 }
+
+public extension UICardStackDelegate {
+    func cardStack(_ cardStack: UICardStackView, didSwipeCardAt index: Int, with direction: SwipeDirection) {}
+    func cardStackDidRunOutOfCards(_ cardStack: UICardStackView) {}
+}
--- Sources/MyFramework/View/UICardStackView.swift
+++ Sources/MyFramework/View/UICardStackView.swift
@@ -2,7 +2,19 @@ import UIKit
 
 public class UICardStackView: UIView {
     
-    weak var dataSource: UICardStackDataSource?
-    weak var delegate: UICardStackDelegate?
+    public weak var dataSource: UICardStackDataSource?
+    public weak var delegate: UICardStackDelegate?
     
+    private var currentIndex = 0
+    
+    public func reloadData() {
+        subviews.forEach { $0.removeFromSuperview() }
+        currentIndex = 0
+        loadNextCards()
+    }
+    
+    private func loadNextCards() {
+        guard let dataSource = dataSource else { return }
+        let cardsNumber = dataSource.numberOfCards(in: self)
+    }
 }
